f3c4532c1d4f71b73daf453fb6348734add627a3
Marc Poppy <poppy.marc@gmail.com>
2020-09-29 02:10:32 +0000

    Automatically backed up by Learn

---
+++ objects.rb
@@ -0,0 +1,19 @@
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.print_all_song_names
+    puts self.all.to_s
+  end
+end
